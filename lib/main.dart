5b88f2e8b0fea5dd3daabae1fcc36651f210feeb
philia <imphilas@gmail.com>
2020-03-07 15:50:34 +0800

    upload to test

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:capo/utils/storage_manager.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await StorageManager.init();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(EasyLocalization(child: CapoApp()));
 }
